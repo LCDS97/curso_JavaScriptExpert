8118f774c850cb91760dda410168ad7e1b935216
LCDS97 <lucas.cds1997@gmail.com>
2021-05-23 19:14:42 -0300

    Stack methods

--- Arrays/js/stack.js
+++ Arrays/js/stack.js
@@ -3,36 +3,45 @@
 // SECTION - Class 11 - Stacks
 
 
-// NOTE Stacks is oriented as objects and introduce the concept of LIFO (Last In First Out)
+// NOTE Stacks is object-oriented and introduce the concept of LIFO (Last In First Out)
 function Stack() {
     var items = []
 
     this.push = function(element) {
         // Add an item in the stack
+        items.push(element)
 
     }
     
     this.pop = function() {
         // Removing an item in top of the stack
+        return items.pop()
     }
+// NOTE In Stacks, the concept is if an item is added to the stack, he will be the first item to get out, in brief, the last item is the first item to get out
 
     this.peek = function() {
         // Retrieve an item that was previously in the top of stack
+
+        return items[items.length - 1]
     }
 
     this.isEmpty = function() {
         //  Will inform if the stack is empty or not
+        return items.length === 0
     }
 
     this.clear = function() {
         // Clear all the stack items
+        items = []
     }
 
     this.size = function() {
         // Will inform the lenght of the stack
+        return items.length
     }
 
     this.print = function() {
         // Print the stack in the console
+        console.log(items.toString())
     }
 }
