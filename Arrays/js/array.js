1e725f8dfd4b56249d607c82d0821fbeb9079926
LCDS97 <lucas.cds1997@gmail.com>
2021-05-23 13:41:16 -0300

    Starting with the array class

---
+++ Arrays/js/array.js
@@ -0,0 +1,23 @@
+// ANCHOR Study code of the extension to see if have children notes
+
+// SECTION Trabalhando com arrays / Working with arrays
+// NOTE Verificar depois sobre estudar e praticar sobre o JS com ES6 / Check later about to study e practice JS with ES6
+
+// NOTE Inicializando um array e irei inserir valores abaixos dentro desse array / Starting an array, and inserting value below and inside this array
+var avgTemp = []
+
+avgTemp[0] = 31.9
+avgTemp[1] = 35.3
+avgTemp[2] = 42
+avgTemp[3] = 38
+avgTemp[4] = 26.5
+
+// NOTE Acessando o array do temp pelo número de indice / Accessing an array named temp with the index number
+console.log(avgTemp[2])
+
+// NOTE Inicializando o array como instancia de um tipo de objeto, não é a melhor prática utilizr assim / Starting an array like a object instance, this is not a good practice to use in projects
+var daysOfWeek = new Array('Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
+
+console.log(daysOfWeek.length)
+
+
