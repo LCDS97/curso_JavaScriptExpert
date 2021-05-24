5dbcfbfd4ff378c32007dd08e0891dea68522b44
Lucas Conceição dos Santos <lucas.cds1997@gmail.com>
2021-05-24 06:43:55 -0300

    Methods of the Queue

--- Arrays/js/queue.js
+++ Arrays/js/queue.js
@@ -5,24 +5,29 @@
 function Queue() {
     var items = [];
 
-    this.enqueue = function() {
+    this.enqueue = function() { // NOTE Add an item to the queue
         // Add an item
+        item.push(element)
     }
 
-    this.dequeue = function() {
+    this.dequeue = function() { // NOTE Remove an item from the queue in the first element in the queue
         // Remove an item
+        return items.shift()
     }
 
-    this.front = function() {
+    this.front = function() { // NOTE Always removing an item from the queue, will return to an index 0 in the queue
         // Return the first item element in the queue
+        return items[0]
     }
 
     this.isEmpty = function() {
         // Verify the queue is empty
+        return items.length === 0
     }
 
-    this.size = function() {
+    this.size = function() { // NOTE Return the size of the queue
         // Return the size of the queue
+        console.log(items.toString())
     }
 
     this.print = function() {
